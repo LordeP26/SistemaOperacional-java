a4068441d22bc121b4dede036e0953fcb78e85e4
LordeP26 <ped.li2608@gmail.com>
2024-11-06 12:55:36 -0300

    Sistemas Operacionais Alfa

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -21,27 +21,23 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		List<Produto> list= new ArrayList<>();
-		
+
+		List<Produto> list = new ArrayList<>();
+
 		int n = 1;
-		
-		/*for(int i = 0; i<n; i++) {
-			
-		list.add(new Comida("Macarrão", 20, 3.00, "1902.19.00", Type.comida));
-		list.add(new Bebidas("Refrigerante", 10, 3.50, "2202.10.00", Type.bebida));
-		list.add(new Lanche("Salgadinho", 8, 15.00, "11905.90.90", Type.lanche));
-		list.add(new Higiene("Pasta de dente", 12, 6.00, "3306.10.00", Type.higiene));
-		}
-		*/
-		
-		
-		System.out.print("Qual item deseja remover do estoque? ");
+
+		System.out.print("Qual item deseja retirar do estoque? ");
 		String name4 = sc.nextLine();
+
+		String nomeAjustado = name4.toLowerCase().replace('m', 'M');
+		String nomeAjustado1 = name4.toLowerCase().replace('r', 'R');
+		String nomeAjustado2 = name4.toLowerCase().replace("chocolate", "Chocolate");
+		String nomeAjustado3 = name4.toLowerCase().replace('p', 'P');
+
 		System.out.print("Quantas unidades? ");
 		int q = sc.nextInt();
-		
-		for(int i = 0; i<n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			String name = "Macarrão";
 			int quantidade = 20;
 			double preco = 3.00;
@@ -57,39 +53,78 @@ public class Program {
 			String name2 = "Chocolate";
 			int quantidade2 = 8;
 			double preco2 = 7.00;
-			String ncm2= "1806.90.00";
+			String ncm2 = "1806.90.00";
 			Type tipo2 = Type.lanche;
 			list.add(new Lanche(name2, quantidade2, preco2, ncm2, tipo2));
 			String name3 = "Pasta de dente";
 			int quantidade3 = 12;
 			double preco3 = 6.00;
-			String ncm3= "3306.10.00";
+			String ncm3 = "3306.10.00";
 			Type tipo3 = Type.higiene;
 			list.add(new Higiene(name3, quantidade3, preco3, ncm3, tipo3));
-			   
+
 		}
-		
+
 		for (Produto p : list) {
-			if(p.getName().equalsIgnoreCase(name4)) {
+			if (p.getName().equalsIgnoreCase(nomeAjustado) || p.getName().equalsIgnoreCase(nomeAjustado1)
+					|| p.getName().equalsIgnoreCase(nomeAjustado2) || p.getName().equalsIgnoreCase(nomeAjustado3)) {
 				p.removeProduto(q);
 			}
 		}
+
+		char r2;
+
+		System.out.print("Deseja retirar mais algum item do estoque (s/n)? ");
+		char r = sc.next().charAt(0);
+		do {
+			if (r == 's' || r == 'S') {
+
+				System.out.print("Qual item deseja retirar do estoque? ");
+				sc.nextLine();
+				String name5 = sc.nextLine();
+
+				String nomeAjustado4 = name5.toLowerCase().replace('m', 'M');
+				String nomeAjustado5 = name5.toLowerCase().replace('r', 'R');
+				String nomeAjustado6 = name5.toLowerCase().replace("chocolate", "Chocolate");
+				String nomeAjustado7 = name5.toLowerCase().replace('p', 'P');
+
+				System.out.print("Quantas unidades? ");
+				int q2 = sc.nextInt();
+
+				for (Produto p : list) {
+					if (p.getName().equalsIgnoreCase(nomeAjustado4) || p.getName().equalsIgnoreCase(nomeAjustado5)
+							|| p.getName().equalsIgnoreCase(nomeAjustado6)
+							|| p.getName().equalsIgnoreCase(nomeAjustado7)) {
+						p.removeProduto(q2);
+					}
+				}
+
+			}
+			System.out.print("Deseja retirar mais algum item do estoque (s/n)? ");
+			r2 = sc.next().charAt(0);
+		} while (r2 != 'n');
+
 		System.out.println();
+		char resp2;
 		System.out.print("Deseja adicionar algum item no estoque (s/n)? ");
 		char resp = sc.next().charAt(0);
 		if (resp == 's') {
-			System.out.print("Qual produto gostaria de adicionar? ");
-			sc.nextLine();
-			String name5 = sc.nextLine();
-			System.out.print("Quantos produtos deseja adicionar? ");
-			int qtt = sc.nextInt();
-			for (Produto p : list) {
-				if(p.getName().equalsIgnoreCase(name5)) {
-					p.addProduto(qtt);
+			do {
+				System.out.print("Qual produto gostaria de adicionar? ");
+				sc.nextLine();
+				String name5 = sc.nextLine();
+				System.out.print("Quantos produtos deseja adicionar? ");
+				int qtt = sc.nextInt();
+				for (Produto p : list) {
+					if (p.getName().equalsIgnoreCase(name5)) {
+						p.addProduto(qtt);
+					}
 				}
-			}
+				System.out.print("Deseja adicionar mais algum item no estoque (s/n)? ");
+				resp2 = sc.next().charAt(0);
+			} while (resp2 != 'n');
 		}
-		
+
 		System.out.println();
 
 		System.out.println("Total em estoque: ");
